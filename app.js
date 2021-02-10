252fee7528643fa48549458b27159b21655ba344
taras131 <mossnabitkana@gmail.com>
2021-02-10 13:38:11 +0300

    d

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ const app = express()
 app.use(express.json({extended: true}))
 app.use("/api/auth", require("./routes/auth.routes"))
 app.use("/api/catalog", require("./routes/catalog.routes"))
-if(process.env.MODE_ENV === `production`){
+if(process.env.NODE_ENV === `production`){
     app.use(`/`, express.static(path.join(__dirname, `client`, `build`)))
     app.get(`*`, (req,res)=>{
-res.sendFile(path.resolve(__dirname,`client`, `build`, index.html))
+        res.sendFile(path.resolve(__dirname,`client`, `build`, index.html))
     })
 }
+
 const PORT = config.get(`port`) || 5000
 const start = async() => {
     try {
